adeee00761480aa3a2e2f1dfe3445bba44cb09e4
NavinGokul27 <138608001+NavinGokul27@users.noreply.github.com>
2023-08-01 20:20:17 +0530

    Add files via upload
    day 2 
    assignment 1

---
+++ even or odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>;
+using namespace std;
+int main()
+{
+    int a=15;
+    if(a%2==0):
+    {
+     cout<<"the number is even:";
+    }
+    else:
+    {
+     Cout<<"the number is odd:";
+    }
+    return 0;
+    
+      
+}
